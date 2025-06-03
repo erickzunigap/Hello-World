94c9446d57ff30802955760fcc5321439aca8f8d
erickzunigap <erick.zuniga@hotmail.com>
2025-06-02 18:46:01 -0600

    Add files via upload

---
+++ Practicas2_v1/src/test/java/Practicas2/QueenApplicationTests.java
@@ -0,0 +1,13 @@
+package Practicas2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
